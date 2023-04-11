a625d5af6f3371686fff1764f369340154638863
HuongNV <huongnv@telsoft.com.vn>
2023-04-11 16:42:30 +0700

    Change api to /msg

--- src/main/java/telsoft/app_cicd/AppCicdApplication.java
+++ src/main/java/telsoft/app_cicd/AppCicdApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class AppCicdApplication {
-    @GetMapping("/message")
+    @GetMapping("/msg")
     public String getMessage() {
         return "Hi, HuongNV";
     }
